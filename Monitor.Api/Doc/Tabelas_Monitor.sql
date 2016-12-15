678b56e0673ac20c128485fcacb4d6cf86731713
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2016-12-15 16:37:07 -0200

    Update Tabelas_Monitor.sql

--- Monitor.Api/Doc/Tabelas_Monitor.sql
+++ Monitor.Api/Doc/Tabelas_Monitor.sql
@@ -1,3 +1,4 @@
+-- Sql Server
 CREATE TABLE [dbo].[LogApi](
 	[Id] [varchar](50) NOT NULL,
 	[Application] [varchar](100) NULL,
@@ -22,3 +23,27 @@ CREATE TABLE [dbo].[LogApi](
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+-- MySql
+
+CREATE TABLE `logapi` (
+  `Id` varchar(50) NOT NULL,
+  `Application` varchar(100) DEFAULT NULL,
+  `Machine` varchar(100) DEFAULT NULL,
+  `RequestContentBody` varchar(1000) DEFAULT NULL,
+  `RequestContentType` varchar(1000) DEFAULT NULL,
+  `RequestHeaders` varchar(1000) DEFAULT NULL,
+  `RequestIpAddress` varchar(1000) DEFAULT NULL,
+  `RequestMethod` varchar(1000) DEFAULT NULL,
+  `RequestRouteData` varchar(1000) DEFAULT NULL,
+  `RequestRouteTemplate` varchar(1000) DEFAULT NULL,
+  `RequestTimestamp` datetime DEFAULT NULL,
+  `RequestUri` varchar(1024) DEFAULT NULL,
+  `ResponseContentBody` varchar(1000) DEFAULT NULL,
+  `ResponseContentType` varchar(1000) DEFAULT NULL,
+  `ResponseHeaders` varchar(1000) DEFAULT NULL,
+  `ResponseStatusCode` int(11) DEFAULT NULL,
+  `ResponseTimestamp` datetime DEFAULT NULL,
+  `User` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
